25abc617b875233b5e5fbbc7d930a8d65d3e45dc
amusing <2455505305@qq.com>
2023-07-23 21:37:11 +0800

    Rotate base om

--- src/integrators/testOM/myOM.h
+++ src/integrators/testOM/myOM.h
@@ -5,10 +5,11 @@ MTS_NAMESPACE_BEGIN
 #define MASK_h27b 0xffffffe0
 #define MASK_l5b 0x0000001f
 #define MASK_l27b 0x07ffffff
+#define PI 3.1415926535
 
-
-template<int omSize, int omDepth>
-class OccupancyMap{
+template <int omSize, int omDepth>
+class OccupancyMap
+{
 private:
     int bom[omSize][omSize][omDepth];
     AABB m_AABB;
@@ -18,39 +19,53 @@ private:
 public:
     OccupancyMap() {}
 
-    inline void clear(){
+    inline void clear()
+    {
         memset(bom, 0, sizeof(bom));
     }
 
-    inline void setAABB(const AABB &aabb){
+    inline void setAABB(const AABB &aabb)
+    {
         m_AABB = aabb;
     }
-    
-    inline void setSize(const Float d){
+
+    inline void setSize(const Float d)
+    {
         m_size = d;
         m_gridSize = m_size / omSize;
     }
 
-    inline void set(int x, int y, int z){
+    inline void set(int x, int y, int z)
+    {
         bom[x][y][(z >> 5) & MASK_l27b] |= 1 << (z & MASK_l5b);
     }
 
-    inline bool check(int x, int y, int z) const {
+    inline void setArray(int *array, int x, int y, int z)
+    {
+        array[(z >> 5) & MASK_l27b] |= 1 << (z & MASK_l5b);
+    }
+
+    inline bool check(int x, int y, int z) const
+    {
         return x >= 0 && x < omSize && y >= 0 && y < omSize && z >= 0 && z < omSize;
     }
 
-    inline bool get(int x, int y, int z) const {
+    inline bool get(int x, int y, int z) const
+    {
         return bom[x][y][(z >> 5) & MASK_l27b] & (1 << (z & MASK_l5b));
     }
 
-    bool rayIntersect(const Ray &ray, Float &nearT) const {
+    bool rayIntersect(const Ray &ray, Float &nearT) const
+    {
         Float farT;
-        if (m_AABB.rayIntersect(ray, nearT, farT)){
+        if (m_AABB.rayIntersect(ray, nearT, farT))
+        {
             Point p = ray.o + ray.d * nearT;
-            int x = (int) floor((p.x - m_AABB.min.x) / m_gridSize + Epsilon);
-            int y = (int) floor((p.y - m_AABB.min.y) / m_gridSize + Epsilon);
-            int z = (int) floor((p.z - m_AABB.min.z) / m_gridSize + Epsilon);
-            while (check(x, y, z)){
+            int x = (int)floor((p.x - m_AABB.min.x) / m_gridSize + Epsilon);
+            int y = (int)floor((p.y - m_AABB.min.y) / m_gridSize + Epsilon);
+            int z = (int)floor((p.z - m_AABB.min.z) / m_gridSize + Epsilon);
+            while (check(x, y, z))
+            {
                 if (get(x, y, z))
                     return true;
                 int sx = ray.d.x > 0 ? 1 : (ray.d.x < 0 ? -1 : 0);
@@ -68,22 +83,28 @@ public:
                 Float ty = sy == 0 ? 1e30f : dy * ray.dRcp.y;
                 Float tz = sz == 0 ? 1e30f : dz * ray.dRcp.z;
 
-                if (tx < ty){
-                    if (tx < tz){
+                if (tx < ty)
+                {
+                    if (tx < tz)
+                    {
                         x = nx;
                         nearT += tx;
                     }
-                    else{
+                    else
+                    {
                         z = nz;
                         nearT += tz;
                     }
                 }
-                else{
-                    if (ty < tz){
+                else
+                {
+                    if (ty < tz)
+                    {
                         y = ny;
                         nearT += ty;
                     }
-                    else{
+                    else
+                    {
                         z = nz;
                         nearT += tz;
                     }
@@ -96,33 +117,101 @@ public:
         return false;
     }
 
-    void testSetAll(){
-        for (int i=0; i<omSize; ++i)
-            for (int j=0; j<omSize; ++j)
-                for (int k=0; k<omSize; ++k)
+    static Quaternion concentricMap(const Point2 &uv)
+    {
+        Float x = uv.x * 2 - 1;
+        Float y = uv.y * 2 - 1;
+        Float phi, r;
+        if (x > -y)
+            if (x > y)
+            {
+                r = x;
+                phi = (PI / 4) * (y / x);
+            }
+            else
+            {
+                r = y;
+                phi = (PI / 4) * (2 - x / y);
+            }
+        else if (x < y)
+        {
+            r = -x;
+            phi = (PI / 4) * (4 + y / x);
+        }
+        else
+        {
+            r = -y;
+            if (y != 0)
+                phi = (PI / 4) * (6 - x / y);
+            else
+                phi = 0;
+        }
+        return normalize(Quaternion::fromDirectionPair(Vector3(0, 0, 1), Vector3(cos(phi) * r, sin(phi) * r, sqrt(1 - r * r))));
+    }
+
+    void generateROMA(OccupancyMap *omarray, Point2 uv)
+    {
+        Quaternion q = concentricMap(uv);
+        SLog(EDebug, "q %f %f %f %f", q.v.x, q.v.y, q.v.z, q.w);
+        for (int x = 0; x < omSize; x++)
+            for (int y = 0; y < omSize; y++)
+            {
+                Vector3 x_start(Float(x - omSize/2), Float(y - omSize/2), 0.5 - omSize/2);
+                Vector3 x_end(Float(x - omSize/2), Float(y - omSize/2), omSize/2 - 1.4);
+                // rotate
+                Vector3 x_start_rot = (q * Quaternion(x_start, 0) * Quaternion(-q.v, q.w)).v + Vector3(omSize/2);
+                Vector3 x_end_rot = (q * Quaternion(x_end, 0) * Quaternion(-q.v, q.w)).v + Vector3(omSize/2);
+                Vector3 v_step = (x_end_rot - x_start_rot) / Float(omSize - 1);
+                for (int i = 0; i < omSize; i++)
+                {
+                    int base_x = (int)floor(x_start_rot.x);
+                    int base_y = (int)floor(x_start_rot.y);
+                    int base_z = (int)floor(x_start_rot.z);
+                    // SLog(EDebug, "base %d %d %d", base_x, base_y, base_z);
+                    if (check(base_x, base_y, base_z) && get(base_x, base_y, base_z))
+                    {
+                        setArray(omarray->bom[x][y], x, y, i);
+                        SLog(EDebug, "set %d %d %d", x, y, i);
+                    }
+                    x_start_rot += v_step;
+                }
+            }
+        // SLog(EDebug, "omarray");
+        // SLog(EDebug, omarray->toString().c_str());
+    }
+
+    void testSetAll()
+    {
+        for (int i = 0; i < omSize; ++i)
+            for (int j = 0; j < omSize; ++j)
+                for (int k = 0; k < omSize; ++k)
                     set(i, j, k);
     }
 
-    void testSetBoxPattern(){
-        for (int i=0; i<omSize; ++i)
-            for (int j=0; j<omSize; ++j)
-                for (int k=0; k<omSize; ++k)
-                    if ( (i+j+k)&1 )
+    void testSetBoxPattern()
+    {
+        for (int i = 0; i < omSize; ++i)
+            for (int j = 0; j < omSize; ++j)
+                for (int k = 0; k < omSize; ++k)
+                    if ((i + j + k) & 1)
                         set(i, j, k);
     }
 
-    void testSetBallPattern(){
-        int c = omSize/2;
-        for (int i=0; i<omSize; ++i)
-            for (int j=0; j<omSize; ++j)
-                for (int k=0; k<omSize; ++k){
-                    int r2 = (i-c)*(i-c)+(j-c)*(j-c)+(k-c)*(k-c);
-                    if ( r2 < omSize*omSize/4 )
+    void testSetBallPattern()
+    {
+        int c = omSize / 2;
+        for (int i = 0; i < omSize; ++i)
+            for (int j = 0; j < omSize; ++j)
+                for (int k = 0; k < omSize; ++k)
+                {
+                    int r2 = (i - c) * (i - c) + (j - c) * (j - c) + (k - c) * (k - c);
+                    if (r2 < omSize * omSize / 4)
                         set(i, j, k);
                 }
     }
 
-    std::string toString() const {
+    std::string toString() const
+    {
         std::ostringstream oss;
         oss << "BOM[" << endl
             << "  AABB: " << m_AABB.toString() << "," << endl
@@ -130,17 +219,17 @@ public:
             << "  gridSize = " << m_gridSize << endl
             << "  OM:\n";
 
-        for (int i=0; i<omSize; ++i)
-            for (int j=0; j<omSize; ++j){
+        for (int i = 0; i < omSize; ++i)
+            for (int j = 0; j < omSize; ++j)
+            {
                 oss << "    ";
-                for (int k=0; k<omSize; ++k)
+                for (int k = 0; k < omSize; ++k)
                     oss << get(i, j, k);
                 oss << endl;
             }
         oss << "  ]";
         return oss.str();
     }
-
 };
 
 MTS_NAMESPACE_END
--- src/integrators/testOM/testOM.cpp
+++ src/integrators/testOM/testOM.cpp
@@ -3,56 +3,62 @@
 
 MTS_NAMESPACE_BEGIN
 
-#define OMSIZE 64
+#define OMSIZE 32
 #define OMDEPTH OMSIZE / 32
 
-class TestOMIntegrater : public SamplingIntegrator {
+class TestOMIntegrater : public SamplingIntegrator
+{
 public:
-MTS_DECLARE_CLASS()
+    MTS_DECLARE_CLASS()
 public:
     /// Initialize the integrator with the specified properties
-    TestOMIntegrater(const Properties &props) : SamplingIntegrator(props) {
+    TestOMIntegrater(const Properties &props) : SamplingIntegrator(props)
+    {
         Spectrum defaultColor;
         defaultColor.fromLinearRGB(1.f, 1.f, 1.f);
         m_color = props.getSpectrum("color", defaultColor);
     }
+
 private:
     Spectrum m_color;
     Float m_maxDist;
     AABB m_baseAABB;
     OccupancyMap<OMSIZE, OMDEPTH> m_om;
+    OccupancyMap<OMSIZE, OMDEPTH> test_om;
 
 public:
     /// Unserialize from a binary data stream
     TestOMIntegrater(Stream *stream, InstanceManager *manager)
-    : SamplingIntegrator(stream, manager) {}
-    
+        : SamplingIntegrator(stream, manager) {}
+
     /// Serialize to a binary data stream
-    void serialize(Stream *stream, InstanceManager *manager) const {
+    void serialize(Stream *stream, InstanceManager *manager) const
+    {
         SamplingIntegrator::serialize(stream, manager);
     }
 
     /// Preprocess function -- called on the initiating machine
     bool preprocess(const Scene *scene, RenderQueue *queue,
-        const RenderJob *job, int sceneResID, int cameraResID, 
-        int samplerResID) {
+                    const RenderJob *job, int sceneResID, int cameraResID,
+                    int samplerResID)
+    {
         SamplingIntegrator::preprocess(scene, queue, job, sceneResID,
-            cameraResID, samplerResID);
+                                       cameraResID, samplerResID);
 
         const AABB &sceneAABB = scene->getAABB();
         /* Find the camera position at t=0 seconds */
-        Point cameraPosition = scene->getSensor()->getWorldTransform()->eval(0).
-        transformAffine(Point(0.0f));
-        m_maxDist = - std::numeric_limits<Float>::infinity();
-        
-        for (int i=0; i<8; ++i)
+        Point cameraPosition = scene->getSensor()->getWorldTransform()->eval(0).transformAffine(Point(0.0f));
+        m_maxDist = -std::numeric_limits<Float>::infinity();
+
+        for (int i = 0; i < 8; ++i)
             m_maxDist = std::max(m_maxDist,
-                (cameraPosition - sceneAABB.getCorner(i)).length());
-        
+                                 (cameraPosition - sceneAABB.getCorner(i)).length());
+
         Point m_min(1e30f), m_max(-1e30f), m_center, m_lcorner;
         auto meshes = scene->getMeshes();
 
-        for (auto m : meshes){
+        for (auto m : meshes)
+        {
             SLog(EInfo, m->toString().c_str());
             m_min.x = std::min(m_min.x, m->getAABB().min.x);
             m_min.y = std::min(m_min.y, m->getAABB().min.y);
@@ -61,36 +67,41 @@ public:
             m_max.y = std::max(m_max.y, m->getAABB().max.y);
             m_max.z = std::max(m_max.z, m->getAABB().max.z);
         }
-        
-        Vector3 d = m_max - m_min;        
+
+        Vector3 d = m_max - m_min;
         Float r = 0;
         for (int i = 0; i < 3; ++i)
             r = std::max(r, d[i]);
         r *= 0.5 * 1.01f;
         m_center = m_min + d / 2.0f;
         m_lcorner = m_center - Vector3(r);
-        m_baseAABB = AABB(m_lcorner, m_lcorner + Vector3(2*r));
+        m_baseAABB = AABB(m_lcorner, m_lcorner + Vector3(2 * r));
 
         m_om.clear();
         m_om.setAABB(m_baseAABB);
-        m_om.setSize(2*r);
+        m_om.setSize(2 * r);
         // m_om.testSetAll();
         m_om.testSetBallPattern();
-
+        test_om.clear();
+        test_om.setAABB(m_baseAABB);
+        test_om.setSize(2 * r);
+        m_om.generateROMA(&test_om, Point2(0.2, 0.2));
         // std::ostringstream oss;
         // oss<<
         // SLog(EDebug, m_om.toString().c_str());
         // SLog(EDebug, m_om.toString().c_str());
-        
+
         return true;
     }
 
     /// Query for an unbiased estimate of the radiance along <tt>r</tt>
-    Spectrum Li(const RayDifferential &r, RadianceQueryRecord &rRec) const {
+    Spectrum Li(const RayDifferential &r, RadianceQueryRecord &rRec) const
+    {
 
         Float nearT;
-        if (m_om.rayIntersect(r, nearT)){
-            return Spectrum(0.8f - nearT/m_maxDist) * m_color;
+        if (test_om.rayIntersect(r, nearT))
+        {
+            return Spectrum(0.8f - nearT / m_maxDist) * m_color;
         }
         // if (rRec.rayIntersect(r)) {
         //     Float distance = rRec.its.t;
